3884cbfa94fbbe77f64b80a94a8a6c84336941c9
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2024-09-26 14:39:43 -0700

    9/26/24 notes

--- Express/04-params-query.js
+++ Express/04-params-query.js
@@ -14,21 +14,54 @@ app.get('/api/products', (req,res)=>{
         return {id, name, age}
     })
     res.json(newProducts)
+    // res. json 
 })
 
 //This is how you set up params for the data query
-
+// anything after : is considered a parameter
 app.get('/api/products/:productID', (req,res)=>{
-    console.log(req.params)
+    console.log(req.query)
     const {productID} = req.params
     const singleProduct = products.find((product) => product.id === Number(productID))
 
     if(!singleProduct){
         return res.status(404).send('Product not found/Item does not exist')
+    }else if(req.query.life =='42'){
+        return res.status(202).send('you have found the easter egg')
     }
     return res.json(singleProduct)
 })
+app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
+    //params are in string format use number to make them a number
+    //there is no limit to how many paramters you can make
+    console.log(req.params)
+    console.log(Number(req.params.productID))
+    res.send(`this product has been reviewd by a person: its the best there is 10/10 would buy again`)
+})
+
+//sets up a  query that you can grab
+app.get('/api/v1/query', (req,res)=>{
+    console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.lenght < 1){
+        return res.status(200).json({success: true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
+})
+
 
 app.listen(5000, ()=>{
     console.log('Server listening on port 5000....')
 })
+
+//query parameter is seperated by a? if more are put it is seperated by a &
